1f33ac1d7d557aa88ac742cf13a3bdd2f1050270
Daniel Le <greenrecyclebin@gmail.com>
2018-02-14 15:44:10 +0800

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,17 +3,17 @@
 
   :url "http://rigsomelight.com/2014/05/01/interactive-programming-flappy-bird-clojurescript.html"
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.293"]
-                 [org.clojure/core.async "0.2.395"]
-                 [cljsjs/react-dom-server "15.3.1-0"]  ;; for sablono
-                 [cljsjs/react-dom "15.3.1-0"] ;; for sablono
-                 [cljsjs/react "15.3.1-0"] ;; for sablono
-                 [sablono "0.7.5"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.9.946"]
+                 [org.clojure/core.async "0.4.474"]
+                 [cljsjs/react-dom-server "16.2.0-3"]  ;; for sablono
+                 [cljsjs/react-dom "16.2.0-3"] ;; for sablono
+                 [cljsjs/react "16.2.0-3"] ;; for sablono
+                 [sablono "0.8.3"]]
 
-  :plugins [[lein-cljsbuild "1.1.4"
+  :plugins [[lein-cljsbuild "1.1.7"
              :exclusions [org.clojure/clojure]]
-            [lein-figwheel "0.5.4-7"]]
+            [lein-figwheel "0.5.14"]]
 
   :clean-targets ^{:protect false} ["resources/public/js/out"
                                     "resources/public/js/flappy_bird_demo.js"
